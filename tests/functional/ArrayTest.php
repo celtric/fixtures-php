5ca83beda082495ed59b80677a2e6568a81989e0
Ricard Osorio <celtric@gmail.com>
2016-05-19 20:41:28 +0200

    add tests for different array configurations

--- tests/functional/ArrayTest.php
+++ tests/functional/ArrayTest.php
@@ -9,7 +9,31 @@ final class ArrayTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function empty_array()
     {
-        $this->assertEquals([], $this->fixture('arrays.empty'));
+        $this->assertEquals([], $this->fixture("arrays.empty"));
+    }
+
+    /** @test */
+    public function scalar_values()
+    {
+        $this->assertEquals([
+            "int" => 123,
+            "float" => 123.456,
+            "string" => "Foo",
+            "bool" => true
+        ], $this->fixture("arrays.scalar_values"));
+    }
+
+    /** @test */
+    public function multidimensional()
+    {
+        $this->assertEquals([
+            "foo" => "bar",
+            "one" => [
+                "two" => [
+                    "three" => "foobar"
+                ]
+            ]
+        ], $this->fixture("arrays.multidimensional"));
     }
 
     //---[ Helpers ]--------------------------------------------------------------------//
