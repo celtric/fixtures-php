0ad6f683f23bee9e6fe7df4d6faca28774e81236
Ricard Osorio <celtric@gmail.com>
2016-05-19 21:32:37 +0200

    add test for complex objects

--- tests/functional/TypeTest.php
+++ tests/functional/TypeTest.php
@@ -4,6 +4,7 @@ namespace Tests\Functional;
 
 use Celtric\Fixtures\Fixtures;
 use Tests\Fixtures\Currency;
+use Tests\Fixtures\Money;
 
 final class TypeTest extends \PHPUnit_Framework_TestCase
 {
@@ -11,6 +12,12 @@ final class TypeTest extends \PHPUnit_Framework_TestCase
     public function simple_object()
     {
         $this->assertEquals(new Currency("EUR"), $this->fixture("currency.euro"));
+    }
+
+    /** @test */
+    public function object_with_typed_fields()
+    {
+        $this->assertEquals(new Money(100, new Currency("EUR")), $this->fixture("money.one_euro"));
     }
 
     //---[ Helpers ]--------------------------------------------------------------------//
