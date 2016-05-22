cb1d95ac9b70b2dc75eb3c6bc98f91eb99de7b99
Ricard Osorio <celtric@gmail.com>
2016-05-22 15:42:01 +0200

    re-organize instantiator

--- src/Celtric/Fixtures/FixtureInstantiator.php
+++ src/Celtric/Fixtures/FixtureInstantiator.php
@@ -22,25 +22,57 @@ final class FixtureInstantiator
      */
     public function instantiate(FixtureDefinition $fixtureDefinition)
     {
-        if ($fixtureDefinition->isNativeValue()) {
-            return $fixtureDefinition->data();
+        switch (true) {
+            case $fixtureDefinition->isNativeValue():
+                return $this->instantiateNativeValue($fixtureDefinition);
+            case $fixtureDefinition->isArray():
+                return $this->instantiateArray($fixtureDefinition);
+            case $fixtureDefinition->isReference():
+                return $this->instantiateReference($fixtureDefinition);
+            default:
+                return $this->instantiateObject($fixtureDefinition);
+        }
+    }
+
+    /**
+     * @param FixtureDefinition $fixtureDefinition
+     * @return int|float|string|bool|null
+     */
+    private function instantiateNativeValue(FixtureDefinition $fixtureDefinition)
+    {
+        return $fixtureDefinition->data();
+    }
+
+    /**
+     * @param FixtureDefinition $fixtureDefinition
+     * @return array
+     */
+    private function instantiateArray(FixtureDefinition $fixtureDefinition)
+    {
+        $instantiatedData = [];
+
+        foreach ($fixtureDefinition->data() as $key => $value) {
+            $instantiatedData[$key] = $this->instantiate($value);
         }
 
-        if ($fixtureDefinition->isArray()) {
-            $instantiatedData = [];
+        return $instantiatedData;
+    }
 
-            foreach ($fixtureDefinition->data() as $key => $value) {
-                $instantiatedData[$key] = $this->instantiate($value);
-            }
+    /**
+     * @param FixtureDefinition $fixtureDefinition
+     * @return mixed
+     */
+    private function instantiateReference(FixtureDefinition $fixtureDefinition)
+    {
+        return $this->instantiate($this->definitionLocator->locate(new FixtureIdentifier($fixtureDefinition->data())));
+    }
 
-            return $instantiatedData;
-        }
-
-        if ($fixtureDefinition->isReference()) {
-            return $this->instantiate(
-                    $this->definitionLocator->locate(new FixtureIdentifier($fixtureDefinition->data())));
-        }
-
+    /**
+     * @param FixtureDefinition $fixtureDefinition
+     * @return mixed
+     */
+    private function instantiateObject(FixtureDefinition $fixtureDefinition)
+    {
         $instance = (new \ReflectionClass($fixtureDefinition->type()))->newInstanceWithoutConstructor();
 
         foreach ($fixtureDefinition->data() as $key => $value) {
