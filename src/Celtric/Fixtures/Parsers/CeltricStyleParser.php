1e076a0ea73079b934b1a14659a2189eb5fb661d
Ricard Osorio <celtric@gmail.com>
2016-08-13 20:15:29 +0200

    refactor celtric parser

--- src/Celtric/Fixtures/Parsers/CeltricStyleParser.php
+++ src/Celtric/Fixtures/Parsers/CeltricStyleParser.php
@@ -27,17 +27,17 @@ final class CeltricStyleParser implements RawDataParser
      */
     public function parse(array $rawData, DefinitionLocator $definitionLocator)
     {
+        $rootType = self::DEFAULT_TYPE;
+
         if (!empty($rawData["root_type"])) {
             if (!is_string($rawData["root_type"])) {
                 throw new \RuntimeException("Root type must be defined as a string");
             }
             $rootType = $rawData["root_type"];
             unset($rawData["root_type"]);
-        } else {
-            $rootType = self::DEFAULT_TYPE;
         }
 
-        return $this->parseData($rawData, $rootType, $definitionLocator);
+        return $this->recursiveParser($rawData, $rootType, $definitionLocator);
     }
 
     /**
@@ -46,7 +46,7 @@ final class CeltricStyleParser implements RawDataParser
      * @param DefinitionLocator $definitionLocator
      * @return FixtureDefinition[]
      */
-    private function parseData($rawData, $defaultType, DefinitionLocator $definitionLocator)
+    private function recursiveParser($rawData, $defaultType, DefinitionLocator $definitionLocator)
     {
         if (!is_array($rawData)) {
             if ($defaultType === "array") {
@@ -70,7 +70,7 @@ final class CeltricStyleParser implements RawDataParser
 
             if ($isMethod) {
                 $parsedData[$key] = $this->definitionFactory->methodCall(
-                        $this->parseData(is_array($value) ? $value : [$value], "array", $definitionLocator));
+                        $this->recursiveParser(is_array($value) ? $value : [$value], "array", $definitionLocator));
                 continue;
             }
 
@@ -79,42 +79,31 @@ final class CeltricStyleParser implements RawDataParser
             } elseif (is_array($value)) {
                 $type = $defaultType;
             } else {
-                $type = $this->resolveType($value);
+                $type = "scalar";
             }
 
-            $parsedValue = $this->parseData($value, $type, $definitionLocator);
-
-            switch (true) {
-                case is_null($parsedValue):
-                    $parsedData[$key] = $this->definitionFactory->null();
-                    break;
-                case is_scalar($parsedValue):
-                    $parsedData[$key] = $this->definitionFactory->scalar($parsedValue);
-                    break;
-                case $type === "array":
-                    $parsedData[$key] = $this->definitionFactory->arr($parsedValue);
-                    break;
-                default:
-                    $parsedData[$key] = $this->definitionFactory->object($type, $parsedValue);
-                    break;
-            }
+            $parsedData[$key] = $this->toDefinition($type, $this->recursiveParser($value, $type, $definitionLocator));
         }
 
         return $parsedData;
     }
 
     /**
-     * @param mixed $value
-     * @return string
+     * @param string $type
+     * @param mixed $parsedValue
+     * @return FixtureDefinition
      */
-    private function resolveType($value)
+    private function toDefinition($type, $parsedValue)
     {
-        $nativeType = strtolower(gettype($value));
-
-        if ($nativeType === "double") {
-            return "float";
+        switch (true) {
+            case is_null($parsedValue):
+                return $this->definitionFactory->null();
+            case is_scalar($parsedValue):
+                return $this->definitionFactory->scalar($parsedValue);
+            case $type === "array":
+                return $this->definitionFactory->arr($parsedValue);
+            default:
+                return $this->definitionFactory->object($type, $parsedValue);
         }
-
-        return $nativeType;
     }
 }
