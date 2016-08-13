ccfa7160b1f200d17509d2be2d431f890ca6e441
Ricard Osorio <celtric@gmail.com>
2016-08-13 16:59:21 +0200

    deprecate generic factory method

--- src/Celtric/Fixtures/FixtureDefinitionFactory.php
+++ src/Celtric/Fixtures/FixtureDefinitionFactory.php
@@ -14,6 +14,7 @@ class FixtureDefinitionFactory
      * @param string $type
      * @param mixed $data
      * @return FixtureDefinition
+     * @deprecated
      */
     public function generic($type, $data)
     {
